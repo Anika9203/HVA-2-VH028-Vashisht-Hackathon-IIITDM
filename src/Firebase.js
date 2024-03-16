8442a9b5b7e93c5fc6fa8df06a155e4cbf8c49a4
Meghana-204 <121662138+Meghana-204@users.noreply.github.com>
2024-03-16 23:45:32 +0530

    firebase deployment setup

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,21 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB78zNPZmf_iEsrBJ7TsE0AKKHjvlx-U70",
-  authDomain: "lms-platform-735f3.firebaseapp.com",
-  projectId: "lms-platform-735f3",
-  storageBucket: "lms-platform-735f3.appspot.com",
-  messagingSenderId: "534622340567",
-  appId: "1:534622340567:web:b50699058188f3c607b792",
-}
+  apiKey: "AIzaSyDhwISN6kwhX8yKSyYeAI48lrB2d2_ZZQU",
+  authDomain: "edu-zen.firebaseapp.com",
+  projectId: "edu-zen",
+  storageBucket: "edu-zen.appspot.com",
+  messagingSenderId: "392189948380",
+  appId: "1:392189948380:web:ec351292eea527aff99f19"
+};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 
 export { db }
